59cdcc9f23ca7b9ce1837eee503c258afb60ef4c
phatdroid94 <phatdroid94@gmail.com>
2021-01-14 18:53:12 +0700

    finish

--- app/src/main/java/com/example/lifecycleactivity24112020/MainActivity2.java
+++ app/src/main/java/com/example/lifecycleactivity24112020/MainActivity2.java
@@ -2,15 +2,29 @@ package com.example.lifecycleactivity24112020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button mBtnNavigate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        mBtnNavigate = findViewById(R.id.buttonNavigateScreen1);
+
+        mBtnNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
         Log.d("BBB","Main2 : onCreate");
     }
     @Override
